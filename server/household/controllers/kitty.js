bc3036aaf99f8d8461aa3afe99693b2d40591ffa
Christophe Olinger <christophe@olinger.eu>
2014-02-10 13:06:14 +0100

    Misse din previous commit

--- server/household/controllers/kitty.js
+++ server/household/controllers/kitty.js
@@ -1,13 +1,37 @@
-exports.all = function(fn) {
-  fn(null, [
-     {'name':'tobi'},
-     {'name':'jane'},
-     {'name':'loki'}
-  ]);
+'use strict';
+
+/**
+ * Load mongo models into mongoose
+ */
+
+require('../models/kitty');
+
+/**
+ * Module dependencies
+ */
+
+var mongoose = require('mongoose'),
+    Kitty = mongoose.model('Kitty');
+
+exports.create = function(req, res) {
+   var kitty = new Kitty(req.body);
+   kitty.save(function(err) {
+      if (err) {
+         console.log(err);
+      }
+
+      return res.send(kitty);
+   });
 };
 
-exports.one = function(fn) {
-  fn(null, [
-     {'name':'tobi'}
-  ]);
+exports.all = function(req, res) {
+    Kitty.find().sort('-created').populate('kitty', 'name').exec(function(err, kitties) {
+        if (err) {
+            res.render('error', {
+                status: 500
+            });
+        } else {
+            res.jsonp(kitties);
+        }
+    });
 };

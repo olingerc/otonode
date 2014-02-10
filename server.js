45aa29ee72fe68ad4bc5f83c1f8bab0013b74517
Christophe Olinger <christophe@olinger.eu>
2014-02-10 09:53:02 +0100

    Take over things from mean

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express =       require('express')
+    , mongoStore = require('connect-mongo')(express)
     , http =        require('http')
     , passport =    require('passport')
     , path =        require('path')
@@ -10,23 +11,63 @@ var express =       require('express')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
+//Initialize system variables
+var config = require('./config/config'),
+    mongoose = require('mongoose');
+
+// Bootstrap db connection
+var db = mongoose.connect(config.db);
+
 /**
  * Configure app
  */
 
 var app = module.exports = express();
 
-app.set('views', __dirname + '/client/js/core');
 app.set('view engine', 'jade');
-app.use(express.logger('dev'));
+app.set('views', __dirname + '/client/js/core');
+
+
+//Not sure from mean
+   app.set('showStackError', true); //Necessary?
+   //Mean uses dynamic helpers. what are those?
+//End not sure
+
+//Taken over from MEAN
+   app.locals.pretty = true;
+    // Should be placed before express.static
+    // To ensure that all assets and data are compressed (utilize bandwidth)
+   app.use(express.compress({
+      filter: function(req, res) {
+         return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+      },
+      // Levels are specified in a range of 0 to 9, where-as 0 is
+      // no compression and 9 is best compression, but slowest
+      level: 9
+   }));
+
+   // Only use logger for development environment
+   if (process.env.NODE_ENV === 'development') {
+      app.use(express.logger('dev'));
+   }
+
+   app.enable('jsonp callback');
+//END taken over
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.cookieParser());
 app.use(express.cookieSession(
     {
-        secret: process.env.COOKIE_SECRET || "E&R:f>+2s70cy~SpZp4%b(7rb`s!PCxSuD3@po)/M)14{gW-;&5mp3te[+#$k^Oq')"
+        secret: config.sessionSecret,
+        store: new mongoStore({
+            db: db.connection.db,
+            collection: config.sessionCollection
+        })
     }));
 
 app.configure('development', 'production', function() {

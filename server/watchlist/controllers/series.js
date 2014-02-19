0794ff6aa7f4159e6ec3f1aeaadb7b7e77ccbfa9
Christophe Olinger <christophe@olinger.eu>
2014-02-19 17:09:42 +0100

    Use async.js

--- server/watchlist/controllers/series.js
+++ server/watchlist/controllers/series.js
@@ -17,7 +17,9 @@ var mongoose = require('mongoose'),
    rootPath = require("../../../config/config").root,
    _ = require("underscore"),
    http = require('http'),
-   fs = require('fs');
+   fs = require('fs'),
+   async = require("async");
+
 
 exports.search = function(req, res) {
    var query = req.query.query;
@@ -28,8 +30,13 @@ exports.search = function(req, res) {
            console.log(err);
            res.send(500, err);
         } else {
-           var showsPrepared = prepareShowsSearchJSON(tvdbres.Data.Series);
-           res.send(showsPrepared);
+           if (tvdbres.Data) {
+              prepareShowsSearchJSON(tvdbres.Data.Series, function(shows) {
+                 res.send(shows);
+              });
+           } else {
+              res.send();
+           }
         }
       });
    } else {
@@ -48,8 +55,10 @@ exports.thumb = function(req, res) {
    });
 };
 
-function prepareShowsSearchJSON(tvdbdata) {
+function prepareShowsSearchJSON(tvdbdata, allShowsPreparedCallback) {
    var showArray = [];
+   if (!_.isArray(tvdbdata)) tvdbdata = [tvdbdata]; //tvdb returns object if only one result
+
    _.each(tvdbdata, function(show) {
       //I only accept results that are featured on IMDB
       if (_.has(show, 'IMDB_ID')) {
@@ -58,21 +67,36 @@ function prepareShowsSearchJSON(tvdbdata) {
          if (!preparedShow.FirstAired) preparedShow.FirstAired = 'NA';
          if (!preparedShow.Network) preparedShow.Network = 'NA';
          showArray.push(preparedShow);
-         if (proxy === '') saveImageFromTVDB(show.seriesid, show.banner);
       }
    });
-   return showArray;
+   async.map(showArray, saveImageFromTVDB, function (err, results) {
+     allShowsPreparedCallback(showArray); //Give back the inital collect, because we did not change it. we just waited for all images to be saved
+   });
 }
 
-function saveImageFromTVDB(seriesid, imageurl) {
-   //http://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js
-   //TODO: do I need to wait for the finish event?
-   try {
-      var request = http.get("http://thetvdb.com/banners/" + imageurl, function(response) {
-         var file = fs.createWriteStream("/var/tmp/" + seriesid + ".jpg");
-         response.pipe(file);
-      });
-   } catch (err) {
-      console.log(err);
-   }
-}
+var saveImageFromTVDB = function(show, imageSavedCallback) {
+   var seriesid = show.seriesid,
+      imageurl = show.banner;
+   var dest = "/var/tmp/" + seriesid + ".jpg";
+   
+   //Check if file exists
+   fs.exists(dest, function(exists) {
+      if (exists) {
+         return imageSavedCallback(null);
+      } else {
+         //It does not. Download it
+         try {
+            var file = fs.createWriteStream(dest);
+            var request = http.get("http://thetvdb.com/banners/" + imageurl, function(response) {
+               response.pipe(file);
+               file.on('finish', function() {
+                  return imageSavedCallback(null);
+               });
+            });
+         } catch (err) {
+            console.log(err);
+            return imageSavedCallback(null);
+         }
+      }
+   });
+};

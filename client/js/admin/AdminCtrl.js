aac914e77bcc3c7a1ae3bffb503b903095e4c261
Christophe Olinger <christophe@linger.eu>
2015-12-21 21:50:57 +0100

    server export

--- client/js/admin/AdminCtrl.js
+++ client/js/admin/AdminCtrl.js
@@ -1,135 +1,142 @@
 angular.module('oto')
-.controller('UsersCtrl',
-['$rootScope', '$scope', 'Users', 'Auth', function($rootScope, $scope, Users, Auth) {
+    .controller('UsersCtrl', ['$rootScope', '$scope', 'Users', 'Auth', function($rootScope, $scope, Users, Auth) {
 
-   /**
-    * Initial values
-    */
-   $scope.loading = true;
-   $scope.userRoles = Auth.userRoles;
-   $scope.master = {};
-   $scope.user = {};
-   $scope.user.role = $scope.userRoles.user;
-   $scope.userFormState = {
-      action: 'add',
-      loadedIndex: null,
-      errorAlreadyExists: false,
-      errorFillAll: false
-   };
+        /**
+         * Initial values
+         */
+        $scope.loading = true;
+        $scope.userRoles = Auth.userRoles;
+        $scope.master = {};
+        $scope.user = {};
+        $scope.user.role = $scope.userRoles.user;
+        $scope.userFormState = {
+            action: 'add',
+            loadedIndex: null,
+            errorAlreadyExists: false,
+            errorFillAll: false
+        };
 
-   Users.getAll(function(res) {
-      $scope.users = res;
-      $scope.loading = false;
-   }, function(err) {
-      $rootScope.error = "Failed to fetch users.";
-      $scope.loading = false;
-   });
+        Users.getAll(function(res) {
+            $scope.users = res;
+            $scope.loading = false;
+        }, function(err) {
+            $rootScope.error = "Failed to fetch users.";
+            $scope.loading = false;
+        });
 
-   $scope.update = function(user) {
-      $scope.master = angular.copy(user);
-   };
+        $scope.update = function(user) {
+            $scope.master = angular.copy(user);
+        };
 
-   $scope.isUnchanged = function(user) {
-      return angular.equals(user, $scope.master);
-   };
+        $scope.isUnchanged = function(user) {
+            return angular.equals(user, $scope.master);
+        };
 
-   $scope.userFormAction = function(user) {
-      if ($scope.userFormState.action === 'add') {
-         $scope.addUser(user);
-      } else {
-         $scope.confirmUpdate();
-      }
-   };
-
-   $scope.addUser = function(user) {
-      Users.add(user,
-         function(res) {
-            $scope.users.push(res);
-            $scope.resetForm();
-         },
-         function(err) {
-            if (err == 'Username already exists') {
-               $scope.userFormState.errorAlreadyExists = true;
+        $scope.userFormAction = function(user) {
+            if ($scope.userFormState.action === 'add') {
+                $scope.addUser(user);
+            } else {
+                $scope.confirmUpdate();
             }
-            else if (err == 'Please fill all the required fields') {
-               $scope.userFormState.errorFillAll = true;
-            }
-            else {
-               console.log(err);
-            }
-         }
-      );
-   };
+        };
 
-   $scope.updateUser = function(index) {
-      $scope.userFormState.action = 'save';
-      $scope.user  = angular.copy($scope.users[index]);
-      $scope.user.role = $scope.userRoles[$scope.user.role.title]
-      $scope.master = angular.copy($scope.user);
-      $scope.userFormState.loadedIndex = index;
-   };
+        $scope.addUser = function(user) {
+            Users.add(user,
+                function(res) {
+                    $scope.users.push(res);
+                    $scope.resetForm();
+                },
+                function(err) {
+                    if (err == 'Username already exists') {
+                        $scope.userFormState.errorAlreadyExists = true;
+                    } else if (err == 'Please fill all the required fields') {
+                        $scope.userFormState.errorFillAll = true;
+                    } else {
+                        console.log(err);
+                    }
+                }
+            );
+        };
 
-   $scope.confirmUpdate = function() {
-      Users.update($scope.user,
-         function(user) {
-            $scope.users[$scope.userFormState.loadedIndex] = user;//$scope.user;
-            $scope.resetForm();
-         },
-         function(err) {
-            if (err == 'Username already exists') {
-               $scope.userFormState.errorAlreadyExists = true;
-            }
-            else if (err == 'Please fill all the required fields') {
-               $scope.userFormState.errorFillAll = true;
-            }
-            else {
-               console.log(err);
-            }
-         });
-   };
+        $scope.updateUser = function(index) {
+            $scope.userFormState.action = 'save';
+            $scope.user = angular.copy($scope.users[index]);
+            $scope.user.role = $scope.userRoles[$scope.user.role.title]
+            $scope.master = angular.copy($scope.user);
+            $scope.userFormState.loadedIndex = index;
+        };
 
-   $scope.resetForm = function() {
-      $scope.user = {};
-      $scope.user.role = $scope.userRoles.user;
-      $scope.userFormState = {
-         action: 'add',
-         loadedIndex: null,
-         errorAlreadyExists: false
-      };
-   };
+        $scope.confirmUpdate = function() {
+            Users.update($scope.user,
+                function(user) {
+                    $scope.users[$scope.userFormState.loadedIndex] = user; //$scope.user;
+                    $scope.resetForm();
+                },
+                function(err) {
+                    if (err == 'Username already exists') {
+                        $scope.userFormState.errorAlreadyExists = true;
+                    } else if (err == 'Please fill all the required fields') {
+                        $scope.userFormState.errorFillAll = true;
+                    } else {
+                        console.log(err);
+                    }
+                });
+        };
 
-   $scope.deleteUser = function(user) {
-      //http://stackoverflow.com/questions/14250642/angularjs-how-to-remove-an-item-from-scope
-      Users.remove(user,
-         function (res) {
-            $scope.users.splice($scope.users.indexOf(user), 1);
-         },
-         function(err) {
-            console.log(err);
-         }
-      );
-   };
+        $scope.resetForm = function() {
+            $scope.user = {};
+            $scope.user.role = $scope.userRoles.user;
+            $scope.userFormState = {
+                action: 'add',
+                loadedIndex: null,
+                errorAlreadyExists: false
+            };
+        };
 
-}]);
+        $scope.deleteUser = function(user) {
+            //http://stackoverflow.com/questions/14250642/angularjs-how-to-remove-an-item-from-scope
+            Users.remove(user,
+                function(res) {
+                    $scope.users.splice($scope.users.indexOf(user), 1);
+                },
+                function(err) {
+                    console.log(err);
+                }
+            );
+        };
+
+    }]);
 
 
 angular.module('oto')
-.controller('DBCtrl',
-['$scope', '$http', function($scope, $http) {
-   $scope.exportok = false;
-   $scope.exporterror = false;
-   $scope.exportprogress = false;
+    .controller('DBCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.exportok = false;
+        $scope.exporterror = false;
+        $scope.exportprogress = false;
 
-   $scope.exportDB = function () {
-      $scope.exportprogress = true;
-      $http.get('/exportdb') //TODO
-      .success(function(response) {
-         $scope.exportprogress = false;
-         $scope.exportok = true;
-      })
-      .error(function(response) {
-         $scope.exportprogress = false;
-         $scope.exporterror = true;
-      });
-   };
-}]);
+        $scope.exportDB = function() {
+            $scope.exportprogress = true;
+            $http.get('/exportdb') //TODO
+                .success(function(response) {
+                    $scope.exportprogress = false;
+                    $scope.exportok = true;
+                })
+                .error(function(response) {
+                    $scope.exportprogress = false;
+                    $scope.exporterror = true;
+                });
+        };
+
+        $scope.exportNotes = function() {
+            $scope.exportprogress = true;
+            $http.get('/api/exportnotes')
+                .success(function(response) {
+                    $scope.exportprogress = false;
+                    $scope.exportok = true;
+                })
+                .error(function(response) {
+                    $scope.exportprogress = false;
+                    $scope.exporterror = true;
+                });
+        };
+    }]);
--- server/notes/routes.js
+++ server/notes/routes.js
@@ -1,88 +1,84 @@
-var accessLevels =   require('../../client/js/core/routingConfig').accessLevels,
-   stacks = require('./controllers/stacks'),
-   cards = require('./controllers/cards');
+var accessLevels = require('../../client/js/core/routingConfig').accessLevels,
+    stacks = require('./controllers/stacks'),
+    cards = require('./controllers/cards');
+    inout = require('./controllers/inout');
 
 module.exports = [
-//STACKS
-   {
-      path: '/api/notes/stacks',
-      httpMethod: 'GET',
-      middleware: [stacks.getall],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/stacks',
-      httpMethod: 'POST',
-      middleware: [stacks.post],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/stacks/:stackid',
-      httpMethod: 'PUT',
-      middleware: [stacks.put],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/stacks/:stackid',
-      httpMethod: 'DELETE',
-      middleware: [stacks.delete],
-      accessLevel: accessLevels.user
-   },
+    //STACKS
+    {
+        path: '/api/exportnotes',
+        httpMethod: 'GET',
+        middleware: [inout.exportnotes],
+        accessLevel: accessLevels.admin
+    }, {
+        path: '/api/notes/stacks',
+        httpMethod: 'GET',
+        middleware: [stacks.getall],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/stacks',
+        httpMethod: 'POST',
+        middleware: [stacks.post],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/stacks/:stackid',
+        httpMethod: 'PUT',
+        middleware: [stacks.put],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/stacks/:stackid',
+        httpMethod: 'DELETE',
+        middleware: [stacks.delete],
+        accessLevel: accessLevels.user
+    },
 
-//CARDS
-   {
-      path: '/api/notes/cards',
-      httpMethod: 'GET',
-      middleware: [cards.getall],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/cards/:cardid',
-      httpMethod: 'GET',
-      middleware: [cards.get],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/cards',
-      httpMethod: 'POST',
-      middleware: [cards.post],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/cards/:cardid',
-      httpMethod: 'PUT',
-      middleware: [cards.put],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/cards/:cardid',
-      httpMethod: 'DELETE',
-      middleware: [cards.delete],
-      accessLevel: accessLevels.user
-   },
-//Att
-   {
-      path: '/upload',
-      httpMethod: 'POST',
-      middleware: [cards.upload],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/deleteatts',
-      httpMethod: 'POST',
-      middleware: [cards.deleteAtts],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/addlink',
-      httpMethod: 'POST',
-      middleware: [cards.addLink],
-      accessLevel: accessLevels.user
-   },
-   {
-      path: '/api/notes/deletelinks',
-      httpMethod: 'POST',
-      middleware: [cards.deleteLinks],
-      accessLevel: accessLevels.user
-   }
+    //CARDS
+    {
+        path: '/api/notes/cards',
+        httpMethod: 'GET',
+        middleware: [cards.getall],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/cards/:cardid',
+        httpMethod: 'GET',
+        middleware: [cards.get],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/cards',
+        httpMethod: 'POST',
+        middleware: [cards.post],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/cards/:cardid',
+        httpMethod: 'PUT',
+        middleware: [cards.put],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/cards/:cardid',
+        httpMethod: 'DELETE',
+        middleware: [cards.delete],
+        accessLevel: accessLevels.user
+    },
+    //Att
+    {
+        path: '/upload',
+        httpMethod: 'POST',
+        middleware: [cards.upload],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/deleteatts',
+        httpMethod: 'POST',
+        middleware: [cards.deleteAtts],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/addlink',
+        httpMethod: 'POST',
+        middleware: [cards.addLink],
+        accessLevel: accessLevels.user
+    }, {
+        path: '/api/notes/deletelinks',
+        httpMethod: 'POST',
+        middleware: [cards.deleteLinks],
+        accessLevel: accessLevels.user
+    }
 ];
